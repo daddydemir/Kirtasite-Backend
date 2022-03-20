24a97b0b9c7fbeb7bd655b9f04e455adb6d90fde
daddydemir <mehmetcakmaktasi47@gmail.com>
2022-03-20 16:21:38 +0300

    modelerdeki eksik kolonlar eklendi.

--- models/stationery.go
+++ models/stationery.go
@@ -4,6 +4,8 @@ type Stationery struct {
 	Id          int     `json:"id"`
 	RoleId      int     `json:"role_id"`
 	CompanyName string  `json:"company_name"`
+	Password    string  `json:"password"`
+	ImagePath   string  `json:"image_path"`
 	Mail        string  `json:"mail"`
 	TelNo       string  `json:"tel_no"`
 	Address     string  `json:"address"`
--- models/user.go
+++ models/user.go
@@ -1,11 +1,12 @@
 package models
 
 type User struct {
-	Id       int    `json:"id"`
-	RoleId   int    `json:"role_id" binding:"required"`
-	Username string `json:"username"`
-	Password string `json:"password"`
-	Mail     string `json:"mail"`
-	TelNo    string `json:"tel_no" binding:"required"`
+	Id        int    `json:"id"`
+	RoleId    int    `json:"role_id" binding:"required"`
+	Username  string `json:"username"`
+	Password  string `json:"password"`
+	ImagePath string `json:"image_path"`
+	Mail      string `json:"mail"`
+	TelNo     string `json:"tel_no" binding:"required"`
 	//UserRole Role   `gorm:"foreignKey:role_id"`
 }
